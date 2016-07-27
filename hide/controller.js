d068efb0021ebe072768159c083469eb1f12b249
Warlock-developer <dawin777@hotmail.com>
2016-07-26 23:04:13 -0500

    ng-hide y ng-show

--- hide/controller.js
+++ hide/controller.js
@@ -1,27 +1,14 @@
-angular.module('myFirstApp',[])
-.controller("FirstController",function($scope,$http){
-	$scope.posts = [];
-	$scope.newPost = {};
-	$http.get("http://jsonplaceholder.typicode.com/posts")
-	.success(function(data){
-		console.log(data);
-		$scope.posts = data;
-	})
-	.error(function(err){
-
-	});
-	$scope.addPost = function(){
-		$http.post("http://jsonplaceholder.typicode.com/posts",{
-			title: $scope.newPost.title,
-			body: $scope.newPost.body,
-			userid: 1
-		})
-		.success(function(data,status,headers,config){
-			$scope.posts.push(data);
-			$scope.newPost = {};
-		})
-		.error(function(error,status,headers,config){
-			console.log(error);
-		});
-	}
-});
+angular.module('MyFirstApp', [])
+        .controller("FirstController", function ($scope, $http) {
+            $scope.posts = [];
+            $scope.loading = true;
+            $http.get("http://jsonplaceholder.typicode.com/posts")
+                    .success(function (data) {
+                        console.log(data);
+                        $scope.posts = data;
+                        $scope.loading = false;
+                    })
+                    .error(function (err) {
+                        $scope.loading = false;
+                    });
+        });
