15f52deaf972c677e0cc5f56c3ccf64b82603ba2
sistemas2 <sistemas2@LAPTOP-PCP7K7VA>
2016-07-28 15:13:23 -0500

    herencia de controladores y rootscope

--- child/controller.js
+++ child/controller.js
@@ -1,9 +1,12 @@
 angular.module("miFirstApp", [])
+        //no se necesita declarar en el scope del controlador la variable
+        //nombre ya que lo hereda del rootscoope.
         .run(function ($rootScope) {
             $rootScope.nombre = "Codigo facilito";
+            //variable para compartir en otros controladores
         })
         .controller("FirstController", function ($scope) {
-            $scope.nombre = "dawin";
+            $scope.nombre = "dawin";//sobreescribo la variable del padre rootscope
         })
         .controller("ChildController", function ($scope) {
 
