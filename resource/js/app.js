d2b787b834a06e686d99649dbc340c90f5bec12d
sistemas2 <sistemas2@LAPTOP-PCP7K7VA>
2016-07-30 12:15:07 -0500

    ultimas practicas

--- resource/js/app.js
+++ resource/js/app.js
@@ -2,11 +2,19 @@
  * Muchos de los usus de angular es consumiendo apiResfull
  * @param {type} param
  */
-angular.module("FinalApp", ["lumx", "ngRoute","ngResource"])
+angular.module("FinalApp", ["lumx", "ngRoute", "ngResource"])
         .config(function ($routeProvider) {
             $routeProvider
                     .when("/", {
                         controller: "MainController",
                         templateUrl: "templates/home.html"
+                    })
+                    .when("/post/:id", {
+                        controller: "PostController",
+                        templateUrl: "templates/post.html"
+                    })
+                    .when("/posts/new", {
+                        controller: "NewPostController",
+                        templateUrl: "templates/post_form.html"
                     })
         });
--- resource/js/controller.js
+++ resource/js/controller.js
@@ -6,5 +6,29 @@ angular.module("FinalApp")
             //query() -> GET /posts -> devuelve arreglo de posts
             $scope.posts = Post.query();
             $scope.users = User.query();
-            
+
+            $scope.removePost = function (post) {
+                Post.delete({id: post.id}, function (data) {
+                    console.log(data);
+                });
+                $scope.posts = $scope.posts.filter(function (element) {
+                    return element.id !== post.id;
+                });
+
+            }
+
+        })
+        .controller("PostController", function ($scope, $resource, $routeParams) {
+            Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
+            $scope.post = Post.get({id: $routeParams.id});
+        })
+        .controller("NewPostController", function ($scope, $resource) {
+            Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
+            $scope.post = {};
+            $scope.title = "Crear post";
+            $scope.savePost = function(){
+                Post.save({data: $scope.post},function(data){
+                    console.log(data);
+                });
+            }
         })
